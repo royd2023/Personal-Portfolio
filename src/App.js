cabecfc754eebb56c92029d85139061c09bcdd34
royd2023 <rdinh2023@gmail.com>
2025-06-05 15:38:18 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1 className="top-left-name"> Roy Dinh </h1>
+      <div className="info">
+        <header>
+          <h1>Hi, I'm Roy</h1>
+          <p>
+            I aspire to work in the field of Software Engineering, specializing in Artificial Intelligence!
+          </p>
+        </header>
+
+        <section>
+          <h2>Languages</h2>
+          <ul>
+            <li>Python</li>
+            <li>JavaScript</li>
+            <li>Java</li>
+            <li>C++</li>
+            <li>C</li>
+            <li>SQL</li>
+            <li>x86-64 Assembly</li>
+            <li>HTML / CSS</li>
+          </ul>
+        </section>
+
+        <section>
+          <h2>Frontend</h2>
+          <ul>
+            <li>React</li>
+          </ul>
+        </section>
+
+        <section>
+          <h2>Backend</h2>
+          <ul>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>Flask</li>
+          </ul>
+        </section>
+
+        <section>
+          <h2>Databases</h2>
+          <ul>
+            <li>PostgreSQL</li>
+          </ul>
+        </section>
+
+        <section>
+          <h2>Tools</h2>
+          <ul>
+            <li>Git</li>
+            <li>Docker</li>
+            <li>VS Code</li>
+            <li>Jupyter Notebook</li>
+          </ul>
+        </section>
+      </div>
+  
+  
+  </div>
   );
 }
 
